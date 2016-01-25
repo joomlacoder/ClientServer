6fb5de6868cccb9b14ddaf311b39bf0740b2212f
Андрей <ignatevandrej@gmail.com>
2016-01-25 18:32:58 +0300

    Закончил работу

--- Client.java
+++ Client.java
@@ -1,5 +1,33 @@
+import java.io.*;
+import java.net.Socket;
+
 /**
  * Created by Andrej on 25.01.2016.
- */
-public class Client {
+ */public class Client {
+    Socket fromServer;
+    BufferedReader in;
+    PrintWriter out;
+    String user, server;
+
+    public static void main(String[] args) throws IOException{
+        Client client = new Client();
+        System.out.println("Создаем клиента...");
+        client.fromServer = new Socket("172.0.0.1", 8080);
+        System.out.println("Соединяемся... ");
+        client.in = new BufferedReader(new InputStreamReader(client.fromServer.getInputStream()));
+        client.out = new PrintWriter(client.fromServer.getOutputStream(), true);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        while ((client.user = reader.readLine()) != null){
+            client.out.print(client.user);
+            client.server = client.in.readLine();
+            System.out.println(client.server);
+            if(client.user.equalsIgnoreCase("exet"))break;
+        }
+
+        client.fromServer.close();
+        client.in.close();
+        client.out.close();
+        reader.close();
+    }
+
 }
--- Server.java
+++ Server.java
@@ -1,5 +1,39 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by Andrej on 25.01.2016.
  */
 public class Server {
+    ServerSocket serverSocket;
+    Socket fromClient;
+    BufferedReader in;
+    PrintWriter out;
+    String input, output;
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Создаем сервер");
+        Server server = new Server();
+        server.serverSocket = new ServerSocket(8080);
+        System.out.println("Ожидаем клиента...");
+        server.fromClient = server.serverSocket.accept();
+        System.out.println("Добавлен клиента.");
+        server.in = new BufferedReader(new InputStreamReader(server.fromClient.getInputStream()));
+        server.out = new PrintWriter(server.fromClient.getOutputStream(), true);
+        System.out.println("Ждем обращения...");
+        while ((server.input = server.in.readLine()) != null){
+            if(server.input.equalsIgnoreCase("exit")) break;
+            server.out.println("S ::: " + server.input);
+            System.out.println(server.input);
+        }
+        server.close();
+    }
+
+    private void close() throws IOException{
+        this.out.close();
+        this.in.close();
+        this.fromClient.close();
+        this.serverSocket.close();
+    }
 }
